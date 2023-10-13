ac8c6d6704d9c8a4d7a3462da80009d6db2734e7
bigdefence <wjdrkdqls12@naver.com>
2023-10-14 05:39:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,9 @@ def main():
 			h=int(detection_bbox[0].height*image.height)+40
 			crop=img_np[y:y+h,x:x+w]
 			st.image(annotated_image, caption="업로드한 이미지", use_column_width=True)
-			img_resized=crop
+			print(1)
+			img_resized=cv2.resize(crop,(350,350))
+			print(2)
 			img_resized=img_resized.astype(np.float32)/255.
 			img_result=[img_resized]
 			img_result=np.array(img_result,dtype=np.float32)
