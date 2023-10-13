a96cd5695e17830762d42894bd69b3ea033ebdd4
bigdefence <wjdrkdqls12@naver.com>
2023-10-14 05:44:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -55,15 +55,10 @@ def main():
 			h=int(detection_bbox[0].height*image.height)+40
 			crop=img_np[y:y+h,x:x+w]
 			st.image(annotated_image, caption="업로드한 이미지", use_column_width=True)
-			print(1)
 			img_resized=cv2.resize(crop,(350,350))
-			print(2)
 			img_resized=img_resized.astype(np.float32)/255.0
-			print(3)
 			img_result=[img_resized]
-			print(4)
 			img_result=np.array(img_result,dtype=np.float32)
-			print(5)
 			preds=model.predict(img_result)
 			print(6)
 	        # AI 외모 분석 진행
