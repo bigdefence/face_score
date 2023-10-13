3495fb3112a03516dd5865ecf0a48cbd9327983f
bigdefence <wjdrkdqls12@naver.com>
2023-10-14 04:26:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image,ImageDraw
-from keras.models import load_model
+import tensorflow as tf
 import mediapipe as mp
-model=load_model('face.h5')
+model=tf.keras.models.load_model('face.h5')
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 mp_drawing_styles = mp.solutions.drawing_styles
